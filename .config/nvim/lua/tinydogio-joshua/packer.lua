6d54527b95394fd7a5a3cb446bbc732d1aee9a72
Joshua Hamilton <joshua@tinydog.io>
2022-12-27 18:14:27 -0500

    Comment out lualine.

--- .config/nvim/lua/tinydogio-joshua/packer.lua
+++ .config/nvim/lua/tinydogio-joshua/packer.lua
@@ -6,7 +6,7 @@ return require('packer').startup(function(use)
   use {'wbthomason/packer.nvim'}
   use {'tpope/vim-surround'}
   use {'ThePrimeagen/vim-be-good', disable = vscode}
-  use {'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }, disable = vscode}
+  -- use {'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }, disable = vscode}
   use {'xiyaowong/nvim-transparent', disable = vscode}
   use {'catppuccin/nvim', as = 'catppuccin', disable = vscode}
 end)
--- .config/nvim/lua/tinydogio-joshua/plugin/init.lua
+++ .config/nvim/lua/tinydogio-joshua/plugin/init.lua
@@ -1,6 +1,6 @@
 local vscode = vim.g.vscode == 1
 
 if not vscode then
-  require("tinydogio-joshua/plugin/lualine-config")
+  -- require("tinydogio-joshua/plugin/lualine-config")
 end
 
