3d17dc71c520aea08f326007d36215784ace2967
Joshua Hamilton <joshua@tinydog.io>
2023-01-24 15:30:55 -0500

    Update grep search function.

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -6,7 +6,9 @@ if not vscode then
   vim.keymap.set('n', '<leader>fj', builtin.buffers, {})
   vim.keymap.set('n', '<leader>fk', builtin.find_files, {})
   vim.keymap.set('n', '<leader>fl', builtin.git_files, {})
-  vim.keymap.set('n', '<leader>f;', builtin.live_grep, {})
+  vim.keymap.set('n', '<leader>f;', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  end)
   vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
   require('telescope').setup{
