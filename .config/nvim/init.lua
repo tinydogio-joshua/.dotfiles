176a64fe68853bef1aa9df633010bd5f4cdb793f
Joshua Hamilton <joshua@tinydog.io>
2023-11-22 21:57:21 -0500

    Adjust theme.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -66,7 +66,7 @@ require('lazy').setup({
     "sainnhe/everforest",
     config = function()
       vim.opt.termguicolors = true
-      vim.g.everforest_background = "soft"
+      vim.g.everforest_background = "medium"
       vim.g.everforest_disable_italic_comment = true
       vim.cmd.colorscheme("everforest")
     end
