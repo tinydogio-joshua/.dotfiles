906dd1c437bacd34d7e9a9ada10073052a5c15e8
Joshua Hamilton <joshua.scott.hamilton@walgreens.com>
2024-01-03 11:27:08 -0500

    Add quick buffer change shortcut.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -189,6 +189,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Buffer List
+vim.keymap.set('n', '<leader>b', ":ls<CR>:b ", { silent = true })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
