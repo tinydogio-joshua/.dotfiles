9e70e2c1d2e81d669b5bcd096c68fb45009e5633
Joshua Hamilton <joshua.scott.hamilton@walgreens.com>
2024-04-22 15:07:36 -0400

    Update padding.

--- .config/nvim/lua/custom/plugins/theme.lua
+++ .config/nvim/lua/custom/plugins/theme.lua
@@ -5,7 +5,8 @@ return {
     require('rose-pine').setup {
       dark_variant = 'moon',
       disable_background = true,
-      variant = 'dawn',
+      variant = 'moon',
+      -- variant = 'dawn',
     }
     vim.cmd.colorscheme 'rose-pine'
   end,
