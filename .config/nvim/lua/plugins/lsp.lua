04afc482fb9a2d3fef3de34cba9bbac557a2f78d
Joshua Hamilton <joshua.scott.hamilton@walgreens.com>
2025-02-20 18:37:28 -0500

    Add rename keybind.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -47,8 +47,9 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			vim.keymap.set("n", "gD", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set({ "n" }, "<leader>rn", vim.lsp.buf.rename, {})
 		end,
 	},
 }
